9c06d370ee71f4f1c8be05dc75dd1b8c63de515b
Olivier Levitt <ungawa14@gmail.com>
2021-03-05 13:16:57 +0100

    Fixes

--- src/main/java/fr/rathesh/springbook/springbooktest/HelloController.java
+++ src/main/java/fr/rathesh/springbook/springbooktest/HelloController.java
@@ -1,5 +1,7 @@
 package fr.rathesh.springbook.springbooktest;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,26 +12,43 @@ import okhttp3.Response;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class HelloController {
     private OkHttpClient client = new OkHttpClient();
-    private HashMap<String, String> data = new HashMap<String, String>();
+    private Map<String, Pokemon> cachePokemon = new HashMap<>();
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    public HelloController()  {
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
 
     @RequestMapping("/")
     public String index() {
         return "Hello World !";
     }
+
     @RequestMapping("/espece/{pokemon}")
-    public String getInfos(@PathVariable String pokemon) throws IOException {
-        if ( data.get("pokemon") != null)
+    public Pokemon getInfos(@PathVariable String pokemon) throws IOException {
+        if ( cachePokemon.get(pokemon) != null)
         {
-            return data.get("pokemon");
+            return cachePokemon.get(pokemon);
         }
         Request request = new Request.Builder()
                 .url("https://pokeapi.co/api/v2/pokemon/"+pokemon)
                 .build();
         Response response = client.newCall(request).execute();
-        return response.body().string();
+
+        // On sait pas pourquoi mais ça marche mieux avec ça
+        try {
+            Thread.sleep(5 * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Pokemon pokemonLu = objectMapper.readValue(response.body().charStream(), Pokemon.class);
+        cachePokemon.put(pokemon, pokemonLu);
+        return pokemonLu;
     }
 }
